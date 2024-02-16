463abf439eacdf00f4aa218cc0f137cbbc12a153
Esger <esgerj@gmail.com>
2024-02-16 17:57:36 +0100

    change name, npm i

--- src/app.js
+++ src/app.js
@@ -1,14 +1,14 @@
 import {
-    inject
+  inject
 } from 'aurelia-framework';
 import { EventAggregator } from 'aurelia-event-aggregator';
 @inject(EventAggregator)
 
 export class App {
 
-    constructor(eventAggregator) {
-        this.ea = eventAggregator;
-        this.message = 'Sling|Puck by ashWare';
-    }
+  constructor(eventAggregator) {
+    this.ea = eventAggregator;
+    this.message = 'Bricks4two | ashWare';
+  }
 
 }
